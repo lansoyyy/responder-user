5e9d4b1117e784c10ab6a44226c54308f35ac720
Lans12345 <olanalans12345@gmail.com>
2023-09-18 09:29:56 +0800

    initial report func

--- lib/screens/pages/add_report_page.dart
+++ lib/screens/pages/add_report_page.dart
@@ -1,7 +1,13 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:responder/services/add_report.dart';
 import 'package:responder/widgets/button_widget.dart';
 import 'package:responder/widgets/textfield_widget.dart';
-
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+import 'package:path/path.dart' as path;
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 import '../../widgets/text_widget.dart';
 
 class AddReportPage extends StatefulWidget {
@@ -12,9 +18,100 @@ class AddReportPage extends StatefulWidget {
 }
 
 class _AddReportPageState extends State<AddReportPage> {
+  @override
+  void initState() {
+    super.initState();
+    determinePosition();
+    addMarker();
+  }
+
   final contactnumberController = TextEditingController();
   final addressController = TextEditingController();
   final captionController = TextEditingController();
+  final nameController = TextEditingController();
+  double lat = 0;
+  double long = 0;
+  bool hasLoaded = false;
+
+  addMarker() {
+    Geolocator.getCurrentPosition().then((position) {
+      setState(() {
+        lat = position.latitude;
+        long = position.longitude;
+
+        hasLoaded = true;
+      });
+    }).catchError((error) {
+      print('Error getting location: $error');
+    });
+  }
+
+  late String idFileName = '';
+
+  late File idImageFile;
+
+  late String idImageURL = '';
+
+  Future<void> uploadImage(String inputSource) async {
+    final picker = ImagePicker();
+    XFile pickedImage;
+    try {
+      pickedImage = (await picker.pickImage(
+          source: inputSource == 'camera'
+              ? ImageSource.camera
+              : ImageSource.gallery,
+          maxWidth: 1920))!;
+
+      idFileName = path.basename(pickedImage.path);
+      idImageFile = File(pickedImage.path);
+
+      try {
+        showDialog(
+          context: context,
+          barrierDismissible: false,
+          builder: (BuildContext context) => Padding(
+            padding: const EdgeInsets.only(left: 30, right: 30),
+            child: AlertDialog(
+                title: Row(
+              children: const [
+                CircularProgressIndicator(
+                  color: Colors.black,
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Text(
+                  'Loading . . .',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'QRegular'),
+                ),
+              ],
+            )),
+          ),
+        );
+
+        await firebase_storage.FirebaseStorage.instance
+            .ref('Document/$idFileName')
+            .putFile(idImageFile);
+        idImageURL = await firebase_storage.FirebaseStorage.instance
+            .ref('Document/$idFileName')
+            .getDownloadURL();
+
+        Navigator.of(context).pop();
+      } on firebase_storage.FirebaseException catch (error) {
+        if (kDebugMode) {
+          print(error);
+        }
+      }
+    } catch (err) {
+      if (kDebugMode) {
+        print(err);
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,56 +123,76 @@ class _AddReportPageState extends State<AddReportPage> {
           color: Colors.white,
         ),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 20,
-            ),
-            TextFieldWidget(
-              label: 'Contact Number',
-              controller: contactnumberController,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextFieldWidget(
-              label: 'Address',
-              controller: addressController,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextFieldWidget(
-              label: 'Caption',
-              controller: captionController,
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            Container(
-              height: 100,
-              width: 300,
-              decoration: const BoxDecoration(
-                color: Colors.grey,
+      body: hasLoaded
+          ? SingleChildScrollView(
+              child: Column(
+                children: [
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextFieldWidget(
+                    label: 'Name',
+                    controller: nameController,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextFieldWidget(
+                    label: 'Contact Number',
+                    controller: contactnumberController,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextFieldWidget(
+                    label: 'Address',
+                    controller: addressController,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextFieldWidget(
+                    label: 'Caption',
+                    controller: captionController,
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      uploadImage('gallery');
+                    },
+                    child: Container(
+                      height: 100,
+                      width: 300,
+                      decoration: BoxDecoration(
+                          color: Colors.grey,
+                          image: idImageURL != ''
+                              ? DecorationImage(
+                                  image: NetworkImage(idImageURL),
+                                  fit: BoxFit.cover)
+                              : null),
+                      child: const Icon(
+                        Icons.add,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  ButtonWidget(
+                    label: 'Continue',
+                    onPressed: () {
+                      previewDialog();
+                    },
+                  ),
+                ],
               ),
-              child: const Icon(
-                Icons.add,
-                color: Colors.white,
-              ),
+            )
+          : const Center(
+              child: CircularProgressIndicator(),
             ),
-            const SizedBox(
-              height: 30,
-            ),
-            ButtonWidget(
-              label: 'Continue',
-              onPressed: () {
-                previewDialog();
-              },
-            ),
-          ],
-        ),
-      ),
     );
   }
 
@@ -92,6 +209,13 @@ class _AddReportPageState extends State<AddReportPage> {
           content: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
+              const SizedBox(
+                height: 20,
+              ),
+              TextFieldWidget(
+                label: 'Name',
+                controller: nameController,
+              ),
               const SizedBox(
                 height: 20,
               ),
@@ -141,6 +265,14 @@ class _AddReportPageState extends State<AddReportPage> {
             ),
             TextButton(
               onPressed: () {
+                addReport(
+                    nameController.text,
+                    contactnumberController.text,
+                    addressController.text,
+                    captionController.text,
+                    idImageURL,
+                    lat,
+                    long);
                 Navigator.pop(context);
               },
               child: TextWidget(
@@ -152,5 +284,42 @@ class _AddReportPageState extends State<AddReportPage> {
         );
       },
     );
+  }
+
+  Future<Position> determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition();
   }
 }
---
+++ lib/services/add_report.dart
@@ -0,0 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+Future addReport(
+    name, contactnumber, address, caption, imageURL, lat, long) async {
+  final docUser = FirebaseFirestore.instance.collection('Reports').doc();
+
+  final json = {
+    'name': name,
+    'address': address,
+    'caption': caption,
+    'status': 'Pending',
+    'userId': FirebaseAuth.instance.currentUser!.uid,
+    'contactnumber': contactnumber,
+    'imageURL': imageURL,
+    'dateTime': DateTime.now(),
+    'lat': lat,
+    'long': long
+  };
+
+  await docUser.set(json);
+}
