607beec3857411bd41cd0400a6e804e1390121a4
Lans12345 <olanalans12345@gmail.com>
2024-03-05 08:32:46 +0800

    added upload id func

--- lib/screens/auth/signup_screen.dart
+++ lib/screens/auth/signup_screen.dart
@@ -1,21 +1,170 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:responder/screens/home_screen.dart';
 import 'package:responder/services/add_user.dart';
 import 'package:responder/widgets/button_widget.dart';
 import 'package:responder/widgets/text_widget.dart';
 import 'package:responder/widgets/textfield_widget.dart';
-
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+import 'package:path/path.dart' as path;
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 import '../../widgets/toast_widget.dart';
 
-class SignupScreen extends StatelessWidget {
+class SignupScreen extends StatefulWidget {
+  const SignupScreen({super.key});
+
+  @override
+  State<SignupScreen> createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
   final emailController = TextEditingController();
+
   final passwordController = TextEditingController();
+
   final nameController = TextEditingController();
+
   final contactnumberController = TextEditingController();
+
   final addressController = TextEditingController();
 
-  SignupScreen({super.key});
+  late String idFileName1 = '';
+
+  late File idImageFile1;
+
+  late String idImageURL1 = '';
+
+  Future<void> uploadImage1(String inputSource) async {
+    final picker = ImagePicker();
+    XFile pickedImage;
+    try {
+      pickedImage = (await picker.pickImage(
+          source: inputSource == 'camera'
+              ? ImageSource.camera
+              : ImageSource.gallery,
+          maxWidth: 1920))!;
+
+      idFileName1 = path.basename(pickedImage.path);
+      idImageFile1 = File(pickedImage.path);
+
+      try {
+        showDialog(
+          context: context,
+          barrierDismissible: false,
+          builder: (BuildContext context) => const Padding(
+            padding: EdgeInsets.only(left: 30, right: 30),
+            child: AlertDialog(
+                title: Row(
+              children: [
+                CircularProgressIndicator(
+                  color: Colors.black,
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Text(
+                  'Loading . . .',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'QRegular'),
+                ),
+              ],
+            )),
+          ),
+        );
+
+        await firebase_storage.FirebaseStorage.instance
+            .ref('Document/$idFileName1')
+            .putFile(idImageFile1);
+        idImageURL1 = await firebase_storage.FirebaseStorage.instance
+            .ref('Document/$idFileName1')
+            .getDownloadURL();
+
+        Navigator.of(context).pop();
+      } on firebase_storage.FirebaseException catch (error) {
+        if (kDebugMode) {
+          print(error);
+        }
+      }
+    } catch (err) {
+      if (kDebugMode) {
+        print(err);
+      }
+    }
+
+    setState(() {});
+  }
+
+  late String idFileName = '';
+
+  late File idImageFile;
+
+  late String idImageURL = '';
+
+  Future<void> uploadImage2(String inputSource) async {
+    final picker = ImagePicker();
+    XFile pickedImage;
+    try {
+      pickedImage = (await picker.pickImage(
+          source: inputSource == 'camera'
+              ? ImageSource.camera
+              : ImageSource.gallery,
+          maxWidth: 1920))!;
+
+      idFileName = path.basename(pickedImage.path);
+      idImageFile = File(pickedImage.path);
+
+      try {
+        showDialog(
+          context: context,
+          barrierDismissible: false,
+          builder: (BuildContext context) => const Padding(
+            padding: EdgeInsets.only(left: 30, right: 30),
+            child: AlertDialog(
+                title: Row(
+              children: [
+                CircularProgressIndicator(
+                  color: Colors.black,
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Text(
+                  'Loading . . .',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'QRegular'),
+                ),
+              ],
+            )),
+          ),
+        );
+
+        await firebase_storage.FirebaseStorage.instance
+            .ref('Document/$idFileName')
+            .putFile(idImageFile);
+        idImageURL = await firebase_storage.FirebaseStorage.instance
+            .ref('Document/$idFileName')
+            .getDownloadURL();
+
+        Navigator.of(context).pop();
+      } on firebase_storage.FirebaseException catch (error) {
+        if (kDebugMode) {
+          print(error);
+        }
+      }
+    } catch (err) {
+      if (kDebugMode) {
+        print(err);
+      }
+    }
+
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,6 +181,72 @@ class SignupScreen extends StatelessWidget {
                 text: 'Register here',
                 fontSize: 24,
                 fontFamily: 'Bold',
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 60, bottom: 10),
+                child: Align(
+                  alignment: Alignment.topLeft,
+                  child: TextWidget(
+                    text: 'Front ID Picture',
+                    fontSize: 12,
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  uploadImage1('gallery');
+                },
+                child: Container(
+                  height: 100,
+                  width: 300,
+                  decoration: BoxDecoration(
+                      color: Colors.grey,
+                      image: idImageURL1 != ''
+                          ? DecorationImage(
+                              image: NetworkImage(idImageURL1),
+                              fit: BoxFit.cover)
+                          : null),
+                  child: const Icon(
+                    Icons.add,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 60, bottom: 10),
+                child: Align(
+                  alignment: Alignment.topLeft,
+                  child: TextWidget(
+                    text: 'Back ID Picture',
+                    fontSize: 12,
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  uploadImage2('gallery');
+                },
+                child: Container(
+                  height: 100,
+                  width: 300,
+                  decoration: BoxDecoration(
+                      color: Colors.grey,
+                      image: idImageURL != ''
+                          ? DecorationImage(
+                              image: NetworkImage(idImageURL),
+                              fit: BoxFit.cover)
+                          : null),
+                  child: const Icon(
+                    Icons.add,
+                    color: Colors.white,
+                  ),
+                ),
               ),
               const SizedBox(
                 height: 20,
@@ -75,7 +290,11 @@ class SignupScreen extends StatelessWidget {
               ButtonWidget(
                 label: 'Signup',
                 onPressed: () {
-                  register(context);
+                  if (idImageURL1 != '' && idImageURL != '') {
+                    register(context);
+                  } else {
+                    showToast('Please upload picture of your ID');
+                  }
                 },
               ),
               const SizedBox(
@@ -96,8 +315,13 @@ class SignupScreen extends StatelessWidget {
       // addUser(nameController.text, contactnumberController.text,
       //     addressController.text, emailController.text);
 
-      addUser(nameController.text, contactnumberController.text,
-          addressController.text, emailController.text);
+      addUser(
+          nameController.text,
+          contactnumberController.text,
+          addressController.text,
+          emailController.text,
+          idImageURL1,
+          idImageURL);
 
       showToast('Account created succesfully!');
       Navigator.of(context).pushReplacement(
--- lib/services/add_user.dart
+++ lib/services/add_user.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-Future addUser(name, contactnumber, address, email) async {
+Future addUser(name, contactnumber, address, email, front, back) async {
   final docUser = FirebaseFirestore.instance
       .collection('Users')
       .doc(FirebaseAuth.instance.currentUser!.uid);
@@ -15,6 +15,8 @@ Future addUser(name, contactnumber, address, email) async {
     'userId': FirebaseAuth.instance.currentUser!.uid,
     'type': 'User',
     'contactnumber': contactnumber,
+    'front': front,
+    'back': back,
   };
 
   await docUser.set(json);
